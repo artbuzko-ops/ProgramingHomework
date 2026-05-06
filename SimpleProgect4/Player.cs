327708820525fec19ad82b5ae25436bf49a21d9f
Artem Buzko <art.buzko@gmail.com>
2026-05-06 18:58:41 +0300

    був зроблений код гравця

--- SimpleProgect4/Player.cs
+++ SimpleProgect4/Player.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SimpleProgect4
 {
-    internal class Class1
+    public class Player
     {
+        public string Name { get; private set; }
+        public ChangingParameters Health { get; private set; }
+        public ChangingParameters Mana { get; private set; }
+        public ChangingParameters Energy { get; private set; }
+        public Inventory Inventory { get; private set; }
+        public Player(string name, int health, int mana, int energy)
+        {
+            Name = name;
+            Health = new ChangingParameters(health);
+            Mana = new ChangingParameters(mana);
+            Energy = new ChangingParameters(energy);
+            Inventory = new Inventory();
+        }
     }
 }
